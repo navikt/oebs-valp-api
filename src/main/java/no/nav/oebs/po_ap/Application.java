5b0124b25bbe69b8f5d6ef684649e4b373d117f2
Agnes Oftung Strandbu <agnes.oftung.strandbu@nav.no>
2026-06-03 15:01:51 +0200

    cleanup(OEBS-2123): remove unused code

--- src/main/java/no/nav/oebs/po_ap/Application.java
+++ src/main/java/no/nav/oebs/po_ap/Application.java
@@ -1,9 +1,6 @@
 package no.nav.oebs.po_ap;
 
-// import no.nav.oebs.po_ap.config.Props;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -13,11 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableSchedulerLock(defaultLockAtMostFor = "PT30S")
 public class 	Application {
 
-	private static final Logger logger = LoggerFactory.getLogger(Application.class);
-
 	public static void main(String[] args) {
-		// Props.setProps();
-
 		SpringApplication.run(Application.class, args);
 	}
 }
